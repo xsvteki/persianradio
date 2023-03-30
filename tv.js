4c2581df35f25ee204ad5cf2050e584d3f8edb55
Ethan Bootehsaz <ethandb2024@gmail.com>
2023-03-30 01:14:47 -0400

    updates

--- tv.js
+++ tv.js
@@ -2,83 +2,96 @@ const tvStations = [];
 
 // Fetch the CSV file and parse it
 fetch('https://raw.githubusercontent.com/ebootehsaz/persianradio/master/tv.csv')
-.then(response => response.text())
-.then(data => {
-  // Parse the CSV data
-  const parsedData = Papa.parse(data).data;
-  // Remove the first row (header values)
-  parsedData.shift();
+  .then(response => response.text())
+  .then(data => {
+    // Parse the CSV data
+    const parsedData = Papa.parse(data).data;
+    // Remove the first row (header values)
+    parsedData.shift();
 
-  // Convert the data into an array of objects
-  parsedData.forEach(row => {
-    tvStations.push({
-      english_name: row[0],
-      farsi_name: row[1],
-      tv_link: row[2],
+    // Convert the data into an array of objects
+    parsedData.forEach(row => {
+      tvStations.push({
+        english_name: row[0],
+        farsi_name: row[1],
+        tv_link: row[2],
+      });
+    });
+
+    // Display stations in list
+    const tvList = document.querySelector('.tv-list');
+    tvStations.forEach(station => {
+      const stationDiv = document.createElement('div');
+      stationDiv.classList.add('station');
+      stationDiv.style.cursor = 'pointer';
+
+      const englishName = document.createElement('div');
+      englishName.classList.add('english-name');
+      englishName.textContent = station.english_name;
+      stationDiv.appendChild(englishName);
+
+      const farsiName = document.createElement('div');
+      farsiName.classList.add('farsi-name');
+      farsiName.textContent = station.farsi_name;
+      stationDiv.appendChild(farsiName);
+
+      stationDiv.addEventListener('click', () => {
+        window.location.href = station.tv_link;
+      });
+
+      tvList.appendChild(stationDiv);
     });
   });
 
-  // Display stations in list
-  const tvList = document.querySelector('.tv-list');
 
-  // Determine number of columns based on screen size
-  let numColumns = 6; // desktop
-  if (window.innerWidth < 768) {
-    numColumns = 3; // mobile
-  }
-
-  // Calculate number of stations per column
-  const numStations = tvStations.length;
-  const stationsPerColumn = Math.ceil(numStations / numColumns);
-
-  // Create station boxes
-  for (let i = 0; i < numColumns; i++) {
-    const column = document.createElement('div');
-    column.classList.add('column');
-    tvList.appendChild(column);
-
-    for (let j = 0; j < stationsPerColumn; j++) {
-      const stationIndex = i * stationsPerColumn + j;
-      if (stationIndex >= numStations) {
-        break;
-      }
-
-      const station = tvStations[stationIndex];
-      const box = document.createElement('div');
-      box.classList.add('box');
-      column.appendChild(box);
-
-      const link = document.createElement('a');
-      link.href = station.tv_link;
-      link.target = '_blank';
-      box.appendChild(link);
-
-      const overlay = document.createElement('div');
-      overlay.classList.add('overlay');
-      link.appendChild(overlay);
-
-      const title = document.createElement('div');
-      title.classList.add('title');
-      title.innerHTML = station.english_name;
-      overlay.appendChild(title);
-
-      const farsiTitle = document.createElement('div');
-      farsiTitle.classList.add('farsi-title');
-      farsiTitle.innerHTML = station.farsi_name;
-      overlay.appendChild(farsiTitle);
-
-// Request fullscreen when the user clicks on the iframe
-      iframe.addEventListener('click', () => {
-        if (iframe.requestFullscreen) {
-          iframe.requestFullscreen();
-        } else if (iframe.webkitRequestFullscreen) {
-          iframe.webkitRequestFullscreen();
-        } else if (iframe.msRequestFullscreen) {
-          iframe.msRequestFullscreen();
-        }
+  function handleSearch() {
+    const searchInput = document.querySelector('#search-input');
+    const searchTerm = searchInput.value.toLowerCase();
+  
+    // Filter the TV stations that match the search term
+    const filteredStations = tvStations.filter(station => {
+      const englishName = station.english_name.toLowerCase();
+      const farsiName = station.farsi_name.toLowerCase();
+  
+      return englishName.includes(searchTerm) || farsiName.includes(searchTerm);
+    });
+  
+    // Clear the current list of stations
+    const tvList = document.querySelector('.tv-list');
+    tvList.innerHTML = '';
+  
+    // Display the filtered list of stations
+    filteredStations.forEach(station => {
+      const stationDiv = document.createElement('div');
+      stationDiv.classList.add('station');
+      stationDiv.style.cursor = 'pointer';
+    
+      const englishName = document.createElement('div');
+      englishName.classList.add('english-name');
+      englishName.textContent = station.english_name;
+      stationDiv.appendChild(englishName);
+    
+      const farsiName = document.createElement('div');
+      farsiName.classList.add('farsi-name');
+      farsiName.textContent = station.farsi_name;
+      stationDiv.appendChild(farsiName);
+    
+      stationDiv.addEventListener('click', () => {
+        window.location.href = station.tv_link;
       });
     
-      
-    }
+      // Add or remove the 'hidden' class based on whether the station matches the search term
+      if (englishName.textContent.toLowerCase().includes(searchTerm) || farsiName.textContent.toLowerCase().includes(searchTerm)) {
+        stationDiv.classList.remove('hidden');
+      } else {
+        stationDiv.classList.add('hidden');
+      }
+    
+      tvList.appendChild(stationDiv);
+    });    
   }
-});
+
+const searchButton = document.querySelector('#search-button');
+searchButton.addEventListener('click', handleSearch);
+
+  
